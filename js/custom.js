671946e5cf0356abfc4340468a2c8c29a5efd942
ggugguiscute <kimgukwoo@gmail.com>
2022-12-16 12:49:15 +0900

    1216 login page

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,13 @@
-/********** Header change Effect **********/
-const header = document.querySelector("#header");
-const stickyHeader = () => {
-  const scry = window.scrollY;
-  if (scry > 0) header.classList.add("active");
-  else header.classList.remove("active");
-};
-
-window.addEventListener("scroll", stickyHeader);
+window.addEventListener("load", function () {
+  /********** Header change Effect **********/
+  const header = document.querySelector("#header");
+  const stickyHeader = () => {
+    const scry = window.scrollY;
+    if (scry > 0) header.classList.add("active");
+    else header.classList.remove("active");
+  };
+  window.addEventListener("scroll", stickyHeader);
+});
 
 /********** Get Products Jason data **********/
 const productsBox = document.querySelector(".products");
--- js/header.js
+++ js/header.js
@@ -1,25 +1,27 @@
-/********** Elements Clone for Mobile **********/
-const mobileMenus = document.querySelector(".mobile-menus");
-const navs = document.querySelector(".nav-lists").cloneNode(true);
-const info = document.querySelector(".info").cloneNode(true);
+window.addEventListener("load", function () {
+  /********** Elements Clone for Mobile **********/
+  const mobileMenus = document.querySelector(".mobile-menus");
+  const navs = document.querySelector(".nav-lists").cloneNode(true);
+  const info = document.querySelector(".info").cloneNode(true);
 
-mobileMenus.appendChild(navs);
-mobileMenus.appendChild(info);
+  mobileMenus.appendChild(navs);
+  mobileMenus.appendChild(info);
 
-/********** Mobile Menu Toggle **********/
-const mobileBtn = document.querySelector(".mobile-btn");
-toggleMobileBtn = (e) => {
-  const target = e.currentTarget;
-  const menuHeight = mobileMenus.scrollHeight; //scrollHeight : 지정 대상의 높이값을 읽어준다.
-  target.classList.toggle("active");
+  /********** Mobile Menu Toggle **********/
+  const mobileBtn = document.querySelector(".mobile-btn");
+  toggleMobileBtn = (e) => {
+    const target = e.currentTarget;
+    const menuHeight = mobileMenus.scrollHeight; //scrollHeight : 지정 대상의 높이값을 읽어준다.
+    target.classList.toggle("active");
 
-  if (target.classList.contains("active")) {
-    target.classList.remove("not-active");
-    mobileMenus.style.height = menuHeight + "px"; //또는 `${menuHeight}px`
-  } else {
-    target.classList.add("not-active");
-    mobileMenus.style.height = 0;
-  }
-};
+    if (target.classList.contains("active")) {
+      target.classList.remove("not-active");
+      mobileMenus.style.height = menuHeight + "px"; //또는 `${menuHeight}px`
+    } else {
+      target.classList.add("not-active");
+      mobileMenus.style.height = 0;
+    }
+  };
 
-mobileBtn.addEventListener("click", toggleMobileBtn);
+  mobileBtn.addEventListener("click", toggleMobileBtn);
+});
