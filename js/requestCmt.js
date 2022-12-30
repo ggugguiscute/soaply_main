4aaa3e9e8e92d81ea738d810d0a54e2572edd18a
ggugguiscute <kimgukwoo@gmail.com>
2022-12-30 12:53:28 +0900

    1230 comment control

--- js/requestCmt.js
+++ js/requestCmt.js
@@ -18,20 +18,22 @@ cmtBtn.addEventListener("click", () => {
 
   // 입력창 작성 체크 끝 : 윗 부분이 완료되면 다음 코드로 진행
   // formData 참조 : https://ko.javascript.info/formdata
-  const formData = new FormData(document.querySelector("form"));
-  fetch(`/main_backend/model/register.php?p_idx=${urlIndex}`, {
-    method: "POST",
-    body: formData,
-  })
+  const formData = new FormData(document.querySelector(".comment-form form"));
+  fetch(
+    `/main_backend/model/cmt_ctrl.php?p_idx=${urlIndex}&req_sign=post_cmt`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  )
     .then((res) => {
-      console.log(res);
-      status = res.status;
+      // console.log(res);
+      // status = res.status;
       return res.json();
     })
     .then((resData) => {
-      // console.log(resData);
       alert(resData.msg);
-      location.href = "/main_project/index.html";
+      location.reload();
     })
     .catch((err) => {
       console.log(err);
