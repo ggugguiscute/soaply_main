5ec86362b47df3456064a002c7ea39707f2c8dfb
ggugguiscute <kimgukwoo@gmail.com>
2023-01-09 12:00:20 +0900

    0109 shop page

--- js/header.js
+++ js/header.js
@@ -1,11 +1,44 @@
 window.addEventListener("load", function () {
   /********** Elements Clone for Mobile **********/
   const mobileMenus = document.querySelector(".mobile-menus");
-  const navs = document.querySelector(".nav-lists").cloneNode(true);
-  const info = document.querySelector(".info").cloneNode(true);
+  // const navs = document.querySelector(".nav-lists").cloneNode(true);
+  // const info = document.querySelector(".info").cloneNode(true);
 
-  mobileMenus.appendChild(navs);
-  mobileMenus.appendChild(info);
+  // mobileMenus.appendChild(navs);
+  // mobileMenus.appendChild(info);
+
+  /********** Change Menu link between main and sub pages **********/
+  //1. 웹 주소에서 index 포함 여부를 파악한다.
+  //2. 포함이 되었을 경우 navs li a의 href는 #
+  //3. 포함이 되지 않았을 경우 navs li a 의 주소를 해당 페이지 주소로 변경
+  //4. 주의할 점은 각 링크가 두 개씩 존재한다는 것
+
+  const pgadr = window.location.href;
+  const homeLink = document.querySelectorAll(".home-link");
+  const shopLink = document.querySelectorAll(".shop-link");
+  const galLink = document.querySelectorAll(".gal-link");
+  // console.log(pgadr.includes("index"));
+  if (pgadr.includes("index")) {
+    homeLink.forEach((item) => {
+      item.setAttribute("href", "#");
+    });
+    shopLink.forEach((item) => {
+      item.setAttribute("href", "#");
+    });
+    galLink.forEach((item) => {
+      item.setAttribute("href", "#");
+    });
+  } else {
+    homeLink.forEach((item) => {
+      item.setAttribute("href", "/main_project/index.html");
+    });
+    shopLink.forEach((item) => {
+      item.setAttribute("href", "/main_project/pages/shop.html");
+    });
+    galLink.forEach((item) => {
+      item.setAttribute("href", "/main_project/pages/gallery.html");
+    });
+  }
 
   /********** Mobile Menu Toggle **********/
   const mobileBtn = document.querySelector(".mobile-btn");
--- js/jquery.js
+++ js/jquery.js
@@ -45,7 +45,8 @@ $(function () {
   $.getJSON("/main_backend/model/get_products.php?qnt=9", getGalleryData);
 
   // navigation moving to target section
-  $(".nav-lists li").on("click", function () {
+  $(".nav-lists li").on("click", function (e) {
+    e.preventDefault(); //a에 적용된 기본 기능 제거(클릭이벤트)
     const targetIdx = $(this).index();
     //console.log(targetIdx);
     const pagePosition = $(".nav-target").eq(targetIdx).offset().top;
