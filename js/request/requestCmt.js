a9ae5c76f0de870f9142efeda40133671643b01d
ggugguiscute <kimgukwoo@gmail.com>
2023-01-13 12:24:30 +0900

    0113 star

--- js/request/requestCmt.js
+++ js/request/requestCmt.js
@@ -33,6 +33,7 @@ cmtBtn.addEventListener("click", () => {
       return res.json();
     })
     .then((resData) => {
+      // console.log(resData);
       alert(resData.msg);
       location.reload();
     })
